92780e8448bf7a7f4d1fe142aabae8042e799994
Hawkynt <github@synthelicz.de>
2026-08-29 13:24:57 +0200

    Register AC-3 and DTS conversion adapters

--- Compression.Lib/AudioAdapterResolver.cs
+++ Compression.Lib/AudioAdapterResolver.cs
@@ -6,11 +6,15 @@ namespace Compression.Lib;
 internal static class AudioAdapterResolver {
   private static readonly WavAudioAdapter Wav = new();
   private static readonly CafAudioAdapter Caf = new();
+  private static readonly Ac3AudioAdapter Ac3 = new();
+  private static readonly DtsAudioAdapter Dts = new();
 
   public static IAudioPcmSource? ResolvePcmSource(IFormatDescriptor descriptor)
     => descriptor as IAudioPcmSource ?? descriptor.Id switch {
       "Wav" => Wav,
       "Caf" => Caf,
+      "Ac3" => Ac3,
+      "Dts" => Dts,
       _ => AudioFormatAdapters.ResolvePcmSource(descriptor),
     };
 
@@ -18,6 +22,8 @@ internal static class AudioAdapterResolver {
     => descriptor as IAudioPcmTarget ?? descriptor.Id switch {
       "Wav" => Wav,
       "Caf" => Caf,
+      "Ac3" => Ac3,
+      "Dts" => Dts,
       _ => AudioFormatAdapters.ResolvePcmTarget(descriptor),
     };
 }
