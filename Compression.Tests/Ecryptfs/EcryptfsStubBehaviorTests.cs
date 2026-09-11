560aa13c5d73218a9a3041fec3fe3a2d753b23c2
Hawkynt <github@synthelicz.de>
2026-09-11 10:36:33 +0200

    Test credential-aware eCryptfs replacement and removal

--- Compression.Tests/Ecryptfs/EcryptfsStubBehaviorTests.cs
+++ Compression.Tests/Ecryptfs/EcryptfsStubBehaviorTests.cs
@@ -38,6 +38,74 @@ public class EcryptfsStubBehaviorTests {
     Assert.That(reader.ExtractContent("correct horse"), Is.EqualTo(content));
   }
 
+  [Test, Category("RoundTrip")]
+  public void CredentialAwareAdd_ReplacesPayload_AndPreservesCipherProfile() {
+    const string password = "correct horse";
+    var replacement = Enumerable.Range(0, 12001).Select(i => (byte)(i * 41 + 3)).ToArray();
+    var descriptor = new EcryptfsFormatDescriptor();
+    var modifier = (IArchiveModifiable)descriptor;
+    using var image = new MemoryStream(CreateImage([1, 2, 3, 4, 5], password, "aes256"));
+
+    image.Position = 0;
+    string? signatureBefore;
+    using (var before = new EcryptfsReader(image)) {
+      signatureBefore = before.PassphraseSignature;
+      Assert.That(before.CipherDescription, Is.EqualTo("AES-256-CBC"));
+    }
+
+    image.Position = 0;
+    modifier.Add(
+      image,
+      [ArchiveInputInfo.InMemory("content.bin", replacement)],
+      new ArchiveMutationOptions { Password = password });
+
+    image.Position = 0;
+    using var reader = new EcryptfsReader(image);
+    Assert.That(reader.CipherDescription, Is.EqualTo("AES-256-CBC"));
+    Assert.That(reader.PassphraseSignature, Is.EqualTo(signatureBefore));
+    Assert.That(reader.ExtractContent(password), Is.EqualTo(replacement));
+  }
+
+  [Test, Category("Sad")]
+  public void CredentialAwareAdd_WrongPassphrase_LeavesOriginalByteIdentical() {
+    var descriptor = new EcryptfsFormatDescriptor();
+    var modifier = (IArchiveModifiable)descriptor;
+    using var image = new MemoryStream(CreateImage(Enumerable.Range(0, 5000).Select(i => (byte)i).ToArray()));
+    var before = image.ToArray();
+
+    image.Position = 0;
+    Assert.Throws<CryptographicException>(() => modifier.Add(
+      image,
+      [ArchiveInputInfo.InMemory("content.bin", [9, 8, 7, 6])],
+      new ArchiveMutationOptions { Password = "wrong horse" }));
+
+    Assert.That(image.ToArray(), Is.EqualTo(before));
+  }
+
+  [Test, Category("Sad")]
+  public void CredentialFreeAdd_IsRejectedInsteadOfGuessingPassword() {
+    var modifier = (IArchiveModifiable)new EcryptfsFormatDescriptor();
+    using var image = new MemoryStream(CreateImage([1, 2, 3]));
+    Assert.Throws<InvalidOperationException>(() => modifier.Add(
+      image,
+      [ArchiveInputInfo.InMemory("content.bin", [4, 5, 6])]));
+  }
+
+  [Test, Category("HappyPath")]
+  public void Remove_ContentBin_LeavesValidEmptyLowerFile() {
+    var descriptor = new EcryptfsFormatDescriptor();
+    var modifier = (IArchiveModifiable)descriptor;
+    using var image = new MemoryStream(CreateImage(Enumerable.Repeat((byte)0x6D, 5000).ToArray()));
+
+    modifier.Remove(image, ["content.bin"]);
+
+    Assert.That(image.Length, Is.EqualTo(8192));
+    image.Position = 0;
+    Assert.That(descriptor.List(image, null), Is.Empty);
+    using var reader = new EcryptfsReader(image);
+    reader.ValidatePassword("correct horse");
+  }
+
   [Test, Category("Sad")]
   public void Extract_WrongPassphrase_IsRejectedBeforePayloadDecryption() {
     using var image = new MemoryStream(CreateImage([1, 2, 3, 4]), writable: false);
