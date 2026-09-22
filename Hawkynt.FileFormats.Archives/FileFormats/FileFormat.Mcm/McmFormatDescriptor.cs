874c043eb832a7adfddfaf421f53c93758734fd9
Hawkynt <github@synthelicz.de>
2026-09-22 09:14:03 +0200

    Expose explicit compression optimization capability

--- Hawkynt.FileFormats.Archives/FileFormats/FileFormat.Mcm/McmFormatDescriptor.cs
+++ Hawkynt.FileFormats.Archives/FileFormats/FileFormat.Mcm/McmFormatDescriptor.cs
@@ -7,7 +7,7 @@ namespace FileFormat.Mcm;
 /// <summary>
 /// Describes mcm format.
 /// </summary>
-public sealed class McmFormatDescriptor : IFormatDescriptor, IStreamFormatOperations, IFormatOptionsSchema {
+public sealed class McmFormatDescriptor : IFormatDescriptor, IStreamFormatOperations, IFormatOptionsSchema , ICompressionOptimizable {
   /// <summary>
   /// Gets the id.
   /// </summary>
