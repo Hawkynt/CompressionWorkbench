76642fd65c8b66cb1814115345d5ad37d1e9b884
Hawkynt <github@synthelicz.de>
2026-09-22 09:00:34 +0200

    Add explicit optimization capability contracts

---
+++ Compression.Registry/OptimizationCapabilities.cs
@@ -0,0 +1,27 @@
+namespace Compression.Registry;
+
+/// <summary>
+/// Central capability discovery for operations that used to be presented under
+/// the ambiguous "Optimize" label.
+/// </summary>
+public static class OptimizationCapabilities {
+  public static bool CanCompress(IFormatDescriptor? descriptor)
+    => descriptor is ICompressionOptimizable;
+
+  public static bool CanCanonicalize(IFormatDescriptor? descriptor)
+    => descriptor is IArchiveCanonicalizable;
+
+  public static bool CanRepack(IFormatDescriptor? descriptor)
+    => descriptor is IArchiveRepackable;
+
+  public static bool CanSortDirectoryEntries(IFormatDescriptor? descriptor)
+    => descriptor is IFilesystemDirectoryOrderer;
+
+  public static bool CanDefragmentExtents(IFormatDescriptor? descriptor)
+    => descriptor is IArchiveDefragmentable && descriptor is IFilesystemBlockMover;
+
+  public static bool CanChangeAllocationGeometry(IFormatDescriptor? descriptor)
+    => descriptor is ILayoutOptimizable
+       && descriptor is IArchiveCreatable
+       && descriptor is IFormatOptionsSchema { OptionsSchema.Count: > 0 };
+}
