5c4c6f6bee04673eb0d5af18e9fd8f8352c8f45e
Hawkynt <github@synthelicz.de>
2026-09-22 09:15:49 +0200

    Expose explicit compression optimization capability

--- Hawkynt.FileFormats.Archives/FileFormats/FileFormat.BriefLz/BriefLzFormatDescriptor.cs
+++ Hawkynt.FileFormats.Archives/FileFormats/FileFormat.BriefLz/BriefLzFormatDescriptor.cs
@@ -6,7 +6,7 @@ namespace FileFormat.BriefLz;
 /// <summary>
 /// Describes the BriefLZ <c>blzpack</c> stream format.
 /// </summary>
-public sealed class BriefLzFormatDescriptor : IFormatDescriptor, IStreamFormatOperations, IFormatOptionsSchema {
+public sealed class BriefLzFormatDescriptor : IFormatDescriptor, IStreamFormatOperations, IFormatOptionsSchema , ICompressionOptimizable {
   /// <summary>
   /// Gets the id.
   /// </summary>
