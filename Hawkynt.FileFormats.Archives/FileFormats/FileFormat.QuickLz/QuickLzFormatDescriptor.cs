27f1111fd5164848b455882c60f02333edab59c0
Hawkynt <github@synthelicz.de>
2026-09-24 11:30:15 +0200

    + advertise safe stream compression optimization

--- Hawkynt.FileFormats.Archives/FileFormats/FileFormat.QuickLz/QuickLzFormatDescriptor.cs
+++ Hawkynt.FileFormats.Archives/FileFormats/FileFormat.QuickLz/QuickLzFormatDescriptor.cs
@@ -8,7 +8,7 @@ namespace FileFormat.QuickLz;
 /// <summary>
 /// Describes quick lz format.
 /// </summary>
-public sealed class QuickLzFormatDescriptor : IFormatDescriptor, IStreamFormatOperations, IFormatOptionsSchema {
+public sealed class QuickLzFormatDescriptor : IFormatDescriptor, IStreamFormatOperations, ICompressionOptimizable, IFormatOptionsSchema {
   /// <summary>
   /// Gets the id.
   /// </summary>
