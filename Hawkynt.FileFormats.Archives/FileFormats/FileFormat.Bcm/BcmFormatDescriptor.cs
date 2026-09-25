b4dd6d0d3d159595b26ab54bfd56bba8d241c37f
Hawkynt <github@synthelicz.de>
2026-09-25 07:01:47 +0200

    - stop advertising unsupported compression optimization

--- Hawkynt.FileFormats.Archives/FileFormats/FileFormat.Bcm/BcmFormatDescriptor.cs
+++ Hawkynt.FileFormats.Archives/FileFormats/FileFormat.Bcm/BcmFormatDescriptor.cs
@@ -25,8 +25,7 @@ public sealed class BcmFormatDescriptor : IFormatDescriptor, IStreamFormatOperat
   /// Gets the capabilities.
   /// </summary>
   public FormatCapabilities Capabilities =>
-    FormatCapabilities.CanExtract | FormatCapabilities.CanCreate | FormatCapabilities.CanTest |
-    FormatCapabilities.SupportsOptimize;
+    FormatCapabilities.CanExtract | FormatCapabilities.CanCreate | FormatCapabilities.CanTest;
   /// <summary>
   /// Gets the default extension.
   /// </summary>
